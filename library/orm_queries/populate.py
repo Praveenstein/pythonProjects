b5b8e3f3190a5a07e7e22bfe67833423ff0b432e
Praveenstein <praveen06061995@gmail.com>
2020-09-23 14:22:55 +0530

    modified for pylint

--- library/orm_queries/populate.py
+++ library/orm_queries/populate.py
@@ -5,17 +5,7 @@ This script contains all the functions to populate the database using
 orm_queries classes, it contains the following functions
 
     * populate
-    * Students
-    * Professors
-    * Authors
-    * Books
-    * BooksAuthor
-    * Staffs
-    * BookItem
-    * StudentActivity
-    * ProfessorActivity
-    * StudentBorrow
-    * ProfessorBorrow
+    * main
 
 This script requires that the following packages be installed within the Python
 environment you are running this script in.
@@ -24,88 +14,96 @@ environment you are running this script in.
 
 """
 
+# Standard import
+import random
+from datetime import date, timedelta
 
+# External import
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import random
-from library.orm_queries.models import *
+from sqlalchemy.ext.declarative import declarative_base
+
+# User Import
+from library.orm_queries.models import Staffs, Department,\
+    Students, Professors, Books, Authors, BooksAuthor, BookItem
+
 
 __author__ = 'praveen@gyandata.com'
+Base = declarative_base()
 
 
 def populate(session):
+    """
+    Function to populate the database
+
+    Parameters
+    ----------
+    session: Session
+            The session object that will be used to populate the database
+    """
 
     # Adding Staffs
-
     staff1 = Staffs(name="Staff-1")
     staff2 = Staffs(name="Staff-2")
     staff3 = Staffs(name="Staff-3")
-    try:
-        session.add_all([staff1, staff2, staff3])
-        session.flush()
-    except Exception as err:
-        session.rollback()
-        print(err)
+
+    # Adding to session
+    session.add_all([staff1, staff2, staff3])
+    session.flush()
 
     # Adding Departments
+    departments = [Department(name="Mechanical"),
+                   Department(name="Computer"),
+                   Department(name="Electrical")]
 
-    mechanical = Department(name="Mechanical")
-    cs = Department(name="Computer")
-    ee = Department(name="Electrical")
-    departments = [mechanical, cs, ee]
-    try:
-        session.add_all(departments)
-        session.flush()
-    except Exception as err:
-        session.rollback()
-        print(err)
+    # Addinng to session
+    session.add_all(departments)
+    session.flush()
 
     # Adding Students, professors, books, authors and book-items for all departments
-
     for department in departments:
         for i in range(10):
-            try:
-                student = Students(name="student" + str(department.name) + str(i),
-                                   doj=date.today() - timedelta(random.randint(30, 600)), department=department)
-                session.add(student)
-                session.commit()
-            except Exception as err:
-                session.rollback()
-                print(err)
+
+            student = Students(name="student" + str(department.name) + str(i),
+                               doj=date.today() - timedelta(random.randint(30, 600)),
+                               department=department)
+            session.add(student)
+            session.commit()
 
         for i in range(3):
-            try:
-                professor = Professors(name="professor" + str(department.name) + str(i), department=department)
-                session.add(professor)
-                session.commit()
-            except Exception as err:
-                session.rollback()
-                print(err)
+
+            professor = Professors(name="professor" + str(department.name) + str(i),
+                                   department=department)
+            session.add(professor)
+            session.commit()
 
         for i in range(10):
-            try:
 
-                book = Books(name="book" + str(department.name) + str(i), quantity=2, department=department)
-                author = Authors(name="Author" + str(department.name) + str(i))
-                ba = BooksAuthor()
-                book_1 = BookItem(dopur=date.today() - timedelta(random.randint(30, 600)),
-                                  dopub=date.today() - timedelta(random.randint(30, 600)),
-                                  price=round((random.random() * 10000), 2),
-                                  edition=1.0, book=book)
-                book_2 = BookItem(dopur=date.today() - timedelta(random.randint(30, 600)),
-                                  dopub=date.today() - timedelta(random.randint(30, 600)),
-                                  price=round((random.random() * 10000), 2),
-                                  edition=2.0, book=book)
-                session.add_all([book, author, ba, book_1, book_2])
-                ba.book = book
-                author.books.append(ba)
-                session.commit()
-            except Exception as err:
-                session.rollback()
-                print(err)
+            # pylint: disable=no-member
+
+            book = Books(name="book" + str(department.name) + str(i), quantity=2,
+                         department=department)
+            author = Authors(name="Author" + str(department.name) + str(i))
+            book_author = BooksAuthor()
+            book_1 = BookItem(dopur=date.today() - timedelta(random.randint(30, 600)),
+                              dopub=date.today() - timedelta(random.randint(30, 600)),
+                              price=round((random.random() * 10000), 2),
+                              edition=1.0, book=book)
+            book_2 = BookItem(dopur=date.today() - timedelta(random.randint(30, 600)),
+                              dopub=date.today() - timedelta(random.randint(30, 600)),
+                              price=round((random.random() * 10000), 2),
+                              edition=2.0, book=book)
+            session.add_all([book, author, book_author, book_1, book_2])
+            book_author.book = book
+            author.books.append(book_author)
+            session.commit()
 
 
 def main():
+    """ The main function to initialize engines and call necessary functions"""
+
+    # pylint: disable=no-member
+    # pylint: disable=invalid-name
 
     url = "mysql+pymysql://root:nebula@localhost/library1?charset=utf8mb4"
     engine = create_engine(url)
