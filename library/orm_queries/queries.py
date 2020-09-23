801b5c8b96de8907900d29233441f8007ef600e7
Praveenstein <praveen06061995@gmail.com>
2020-09-23 15:15:04 +0530

    modified for pylint

--- library/orm_queries/queries.py
+++ library/orm_queries/queries.py
@@ -2,7 +2,8 @@
 """ Module to do some transactions with the library database
 
 This script contains all the code details required to perform the
-orm_queries based SQL operations, and do some transactions on the library database, it contains the following functions
+orm_queries based SQL operations, and do some transactions on the
+library database, it contains the following functions
 
     * log
     * student_issue
@@ -17,7 +18,8 @@ orm_queries based SQL operations, and do some transactions on the library databa
 This script requires that the following packages be installed within the Python
 environment you are running this script in.
 
-    * sqlalchemy - Package used to connect to a database and do SQL operations using orm_queries.
+    * sqlalchemy - Package used to connect to a database and do SQL
+      operations using orm_queries.
     * library_class - orm_queries classes for the library database.
     * json - To deal with Json Files.
     * logging - To log Errors.
@@ -25,22 +27,34 @@ environment you are running this script in.
 
 """
 
-
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from library.orm_queries.models import *
-
+# Standard import
+from datetime import date
 import json
 import logging
 import logging.config
+
+# External import
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+# User Import
+from library.orm_queries.models import Staffs, Department, \
+    Students, Professors, Books, BookItem, MyEnum, StudentActivity, \
+    ProfessorActivity, StudentBorrow, ProfessorBorrow
+
+
+__author__ = 'praveen@gyandata.com'
+Base = declarative_base()
 
 
 def log():
     """
     Creates a custom logger from the configuration dictionary
     """
-    with open('D:\\Profession\\Intern\\Assignments\\master_repo\\pythonProjects\\configs\\analyse_log.json', 'r') as f:
-        config = json.load(f)
+    with open('D:\\Profession\\Intern\\Assignments\\master_repo\\'
+              'pythonProjects\\configs\\analyse_log.json', 'r') as file:
+        config = json.load(file)
         logging.config.dictConfig(config)
     global logger
     logger = logging.getLogger(__name__)
@@ -65,9 +79,9 @@ def student_issue(session, staff, s_id, b_id):
     print("\nIssuing\n")
 
     # Asserting the parameters
-    assert type(staff) is int, "Staff ID should be integer"
-    assert type(s_id) is int, "Student ID should be integer"
-    assert type(b_id) is list, "Books should be a list"
+    assert isinstance(staff, int), "Staff ID should be integer"
+    assert isinstance(s_id, int), "Student ID should be integer"
+    assert isinstance(b_id, list), "Books should be a list"
 
     # Getting Student object
     student = session.query(Students).filter_by(reg_id=s_id).first()
@@ -84,21 +98,24 @@ def student_issue(session, staff, s_id, b_id):
         raise AttributeError("Staff not in DB")
 
     # Getting all book-item objects
-    book_list = session.query(BookItem).filter(BookItem.bar_code.in_(b_id)).all()
+    book_list = session.query(BookItem). \
+        filter(BookItem.bar_code.in_(b_id)).all()
 
     if (not book_list) or (len(b_id) != len(book_list)):
         # If books not in database, then raise attribute error
         raise AttributeError("Book item not in DB")
 
-    for j in range(len(book_list)):
-        if book_list[j].status != MyEnum.AVAILABLE:
+    for book_item in book_list:
+        if book_item.status != MyEnum.AVAILABLE:
             # If book is not available then raise attribute error
             raise AttributeError("Book item not Available")
         # If all books are available, then their status are changed
-        book_list[j].status = MyEnum.UNAVAILABLE
+        book_item.status = MyEnum.UNAVAILABLE
 
     # Creating a student activity object to enter transaction details
-    student_activity = StudentActivity(doi=date.today(), student=student, staff=staff)
+    student_activity = StudentActivity(doi=date.today(),
+                                       student=student,
+                                       staff=staff)
     session.add(student_activity)
 
     # Adding all the book items to student activity
@@ -109,8 +126,8 @@ def student_issue(session, staff, s_id, b_id):
     print(f"Student: {student.name}\n")
     print(f"Student_ Activity Id: {student_activity.trans_id}\n")
     print(f"Date Of Issue: {student_activity.doi}\n")
-    for j in range(len(book_list)):
-        print("Name of book: ", book_list[j].book.name)
+    for book_item in book_list:
+        print("Name of book: ", book_item.book.name)
 
 
 def professor_issue(session, staff, p_id, b_id):
@@ -132,12 +149,13 @@ def professor_issue(session, staff, p_id, b_id):
     print("\nIssuing\n")
 
     # Asserting the parameters
-    assert type(staff) is int, "Staff ID should be integer"
-    assert type(p_id) is int, "Professor ID should be integer"
-    assert type(b_id) is list, "Books should be a list"
+    assert isinstance(staff, int), "Staff ID should be integer"
+    assert isinstance(p_id, int), "Professor ID should be integer"
+    assert isinstance(b_id, list), "Books should be a list"
 
     # Getting Professor object
-    professor = session.query(Professors).filter_by(employee_code=p_id).first()
+    professor = session.query(Professors).\
+        filter_by(employee_code=p_id).first()
 
     if not professor:
         # If Professor id is not in database, then raises attribute error
@@ -149,21 +167,24 @@ def professor_issue(session, staff, p_id, b_id):
         raise AttributeError("Staff not in DB")
 
     # Getting all book-item objects
-    book_list = session.query(BookItem).filter(BookItem.bar_code.in_(b_id)).all()
+    book_list = session.query(BookItem).\
+        filter(BookItem.bar_code.in_(b_id)).all()
 
     if (not book_list) or (len(b_id) != len(book_list)):
         # If books not in database, then raise attribute error
         raise AttributeError("Book item not in DB")
 
-    for j in range(len(book_list)):
-        if book_list[j].status != MyEnum.AVAILABLE:
+    for book_item in book_list:
+        if book_item.status != MyEnum.AVAILABLE:
             # If book is not available then raise attribute error
             raise AttributeError("Book item not Available")
         # If all books are available, then their status are changed
-        book_list[j].status = MyEnum.UNAVAILABLE
+        book_item.status = MyEnum.UNAVAILABLE
 
     # Creating a professor activity object to enter transaction details
-    professor_activity = ProfessorActivity(doi=date.today(), professor=professor, staff=staff)
+    professor_activity = ProfessorActivity(doi=date.today(),
+                                           professor=professor,
+                                           staff=staff)
     session.add(professor_activity)
 
     # Adding all the book items to student activity
@@ -174,8 +195,8 @@ def professor_issue(session, staff, p_id, b_id):
     print(f"Professor: {professor.name}\n")
     print(f"Professor_ Activity Id: {professor_activity.trans_id}\n")
     print(f"Date Of Issue: {professor_activity.doi}\n")
-    for j in range(len(book_list)):
-        print("Name of book: ", book_list[j].book.name)
+    for book_item in book_list:
+        print("Name of book: ", book_item.book.name)
 
 
 def student_returning(session, b_id, lost=False, tampered=False):
@@ -196,26 +217,30 @@ def student_returning(session, b_id, lost=False, tampered=False):
     """
     print("\nReturning\n")
 
-    # Assrting the paramters
-    assert type(b_id) is list, "Books should be a list"
-    assert type(lost) is bool, "Lost has to be boolean"
-    assert type(tampered) is bool, "Tampered has to be boolean"
+    # Asserting the parameters
+    assert isinstance(b_id, list), "Books should be a list"
+    assert isinstance(lost, bool), "Lost has to be boolean"
+    assert isinstance(tampered, bool), "Tampered has to be boolean"
 
     # Getting list of book objects
-    book_list = session.query(BookItem).filter(BookItem.bar_code.in_(b_id)).all()
+    book_list = session.query(BookItem).\
+        filter(BookItem.bar_code.in_(b_id)).all()
     if (not book_list) or (len(b_id) != len(book_list)):
         # If books not in database, then raise attribute error
         raise AttributeError("Book item not in DB")
 
     for i in range(len(book_list)):
-        # Getting list of student borrow where the given book item occured
-        student_borrow = session.query(StudentBorrow).filter(StudentBorrow.book_item == book_list[i]).\
-                                    order_by(StudentBorrow.student_trans_id.desc()).first()
+        # Getting list of student borrow where the given book item occurred
+        student_borrow = session.query(StudentBorrow).\
+            filter(StudentBorrow.book_item == book_list[i]). \
+            order_by(StudentBorrow.student_trans_id.desc()).first()
         if not student_borrow:
-            # If book did not occure in any previous transaction, then raise an attribute error
+            # If book did not occur in any previous transaction,
+            # then raise an attribute error
             raise AttributeError("Book Not Found in Any Borrowed Transaction")
         if lost:
-            # If book is lost then, status is changed to lost and student has to pay fine
+            # If book is lost then, status is changed to lost and
+            # student has to pay fine
             book_list[i].status = MyEnum.LOST
             print("\npay Fine\n")
         else:
@@ -225,7 +250,8 @@ def student_returning(session, b_id, lost=False, tampered=False):
         if tampered:
             # If book is tampered
             if not book_list[i].tampered:
-                # if the book item was not tampered previously, then the tampered is changed to true
+                # if the book item was not tampered previously,
+                # then the tampered is changed to true
                 print("\npay Fine\n")
                 book_list[i].tampered = True
 
@@ -254,26 +280,30 @@ def professor_returning(session, b_id, lost=False, tampered=False):
     """
     print("\nReturning\n")
 
-    # Assrting the paramters
-    assert type(b_id) is list, "Books should be a list"
-    assert type(lost) is bool, "Lost has to be boolean"
-    assert type(tampered) is bool, "Tampered has to be boolean"
+    # Asserting the parameters
+    assert isinstance(b_id, list), "Books should be a list"
+    assert isinstance(lost, bool), "Lost has to be boolean"
+    assert isinstance(tampered, bool), "Tampered has to be boolean"
 
     # Getting list of book objects
-    book_list = session.query(BookItem).filter(BookItem.bar_code.in_(b_id)).all()
+    book_list = session.query(BookItem).\
+        filter(BookItem.bar_code.in_(b_id)).all()
     if (not book_list) or (len(b_id) != len(book_list)):
         # If books not in database, then raise attribute error
         raise AttributeError("Book item not in DB")
 
     for i in range(len(book_list)):
-        # Getting list of professor borrow where the given book item occured
-        professor_borrow = session.query(ProfessorBorrow).filter(ProfessorBorrow.book_item == book_list[i]).\
-                                    order_by(ProfessorBorrow.professor_trans_id.desc()).first()
+        # Getting list of professor borrow where the given book item occurred
+        professor_borrow = session.query(ProfessorBorrow).\
+            filter(ProfessorBorrow.book_item == book_list[i]). \
+            order_by(ProfessorBorrow.professor_trans_id.desc()).first()
         if not professor_borrow:
-            # If book did not occure in any previous transaction, then raise an attribute error
+            # If book did not occurred in any previous transaction,
+            # then raise an attribute error
             raise AttributeError("Book Not Found in Any Borrowed Transaction")
         if lost:
-            # If book is lost then, status is changed to lost and professor has to pay fine
+            # If book is lost then, status is changed to lost and
+            # professor has to pay fine
             book_list[i].status = MyEnum.LOST
         else:
             # If book is not lost, then status is changed to available
@@ -282,7 +312,8 @@ def professor_returning(session, b_id, lost=False, tampered=False):
         if tampered:
             # If book is tampered
             if not book_list[i].tampered:
-                # if the book item was not tampered previously, then the tampered is changed to true
+                # if the book item was not tampered previously,
+                # then the tampered is changed to true
                 print("\npay Fine\n")
                 book_list[i].tampered = True
 
@@ -305,7 +336,7 @@ def impact(session, dep):
     """
 
     # Asserting parameters
-    assert type(dep) is int, "Department Id should be integer"
+    assert isinstance(dep, int), "Department Id should be integer"
 
     # Getting department object
     department = session.query(Department).filter_by(dept_id=dep).first()
@@ -313,25 +344,29 @@ def impact(session, dep):
         # If department is not in database, raise attribute error
         raise AttributeError("Department not in DB")
 
-    # Finding transactions in which the book of given department occured, using join
-    query = session.query(Books.isbn_id, Books.name, Department.name, BookItem.bar_code,
+    # Finding transactions in which the book of given department occurRed, using join
+    query = session.query(Books.isbn_id, Books.name,
+                          Department.name, BookItem.bar_code,
                           StudentBorrow.student_trans_id)
     query = query.join(Department).join(BookItem).join(StudentBorrow)
     results = query.filter(Department.dept_id == dep).all()
     if not results:
-        # If nno activity was found, it raise an attribute error
+        # If no activity was found, it raise an attribute error
         raise AttributeError("No Activity For this Department's Books")
 
     data = []
     for result in results:
-        trans = session.query(StudentActivity).filter_by(trans_id=result[-1]).first()
+        trans = session.query(StudentActivity).\
+            filter_by(trans_id=result[-1]).first()
         if trans.student.department.dept_id != dep:
             # Looping through the results and getting details
             # Of those students who are not from the given department
-            data.append((trans.trans_id, trans.student.name, trans.student.department.name))
+            data.append((trans.trans_id, trans.student.name,
+                         trans.student.department.name))
 
     for i in data:
-        print(f"Transaction Id: {i[0]}, Student name: {i[1]}, student department: {i[2]}")
+        print(f"Transaction Id: {i[0]}, Student name: {i[1]},"
+              f" student department: {i[2]}")
 
 
 def check_status(session, book_id):
@@ -348,42 +383,45 @@ def check_status(session, book_id):
     print("\nChecking\n")
 
     # Asserting the parameters
-    assert type(book_id) is int, "Book Id should be integer"
+    assert isinstance(book_id, int), "Book Id should be integer"
 
     # Getting the book object
-    book_object = session.query(Books).filter(Books.isbn_id == book_id).first()
+    book_object = session.query(Books).\
+        filter(Books.isbn_id == book_id).first()
     if not book_object:
         # If book id is not in database, raise an attribute error
         raise AttributeError("Book not in DB")
 
-    # Getting list of all book items assocaiated with the given book object
+    # Getting list of all book items associated with the given book object
     book_items = book_object.book_items
     for item in book_items:
         # Setting due date to none
-        dd = None
+        due_date = None
         if item.status == MyEnum.UNAVAILABLE:
             # If status of book is unavailable, getting student borrow object
-            student_borrow = session.query(StudentBorrow).filter(StudentBorrow.book_item == item).\
-                                        order_by(StudentBorrow.student_trans_id.desc()).first()
+            student_borrow = session.query(StudentBorrow).\
+                filter(StudentBorrow.book_item == item). \
+                order_by(StudentBorrow.student_trans_id.desc()).first()
             if student_borrow:
                 # if student borrow is not none, then
                 # assigning the due date to the due date from the student borrow
-                dd = student_borrow.dd
+                due_date = student_borrow.dd
             else:
                 # If student borrow is none, then checking with the professor borrow
-                professor_borrow = session.query(ProfessorBorrow).\
-                    filter(ProfessorBorrow.book_item == item).\
+                professor_borrow = session.query(ProfessorBorrow). \
+                    filter(ProfessorBorrow.book_item == item). \
                     order_by(ProfessorBorrow.professor_trans_id.desc()).first()
                 if professor_borrow:
                     # if professor borrow is not none, then
                     # assigning the due date to the due date from the professor borrow
-                    dd = professor_borrow.dd
+                    due_date = professor_borrow.dd
 
             print(
                 f"\nBook Name: {book_object.name} - Bard Code: {item.bar_code}"
-                f" is {item.status.name} and is due return on: {dd}\n")
+                f" is {item.status.name} and is due return on: {due_date}\n")
         else:
-            print(f"\nBook Name: {book_object.name} - Bard Code: {item.bar_code} is {item.status.name}\n")
+            print(f"\nBook Name: {book_object.name} - Bard Code: {item.bar_code}"
+                  f" is {item.status.name}\n")
 
 
 def test(session_factory):
@@ -391,6 +429,8 @@ def test(session_factory):
     Function to do some testing with random issues and returns
 
     """
+    # pylint: disable=too-many-branches
+    # pylint: disable=too-many-statements
     session = session_factory()
     try:
         # Issuing book (id-[1]) to student (id-1) by staff (id-1)
@@ -432,14 +472,11 @@ def test(session_factory):
         logger.error(err)
     finally:
         session.close()
-    
+
     session = session_factory()
-    try:
-        book = session.query(BookItem).filter_by(bar_code=2).first()
-        print(book.status)
-    except:
-        session.rollback()
-        session.close()
+    book = session.query(BookItem).filter_by(bar_code=2).first()
+    print(book.status)
+    session.close()
 
     session = session_factory()
     try:
@@ -684,6 +721,10 @@ def main():
     """
     Main function to call the required functions and do some testing
     """
+
+    # pylint: disable=no-member
+    # pylint: disable=invalid-name
+
     # Creating the logging object
     log()
     url = "mysql+pymysql://root:nebula@localhost/library1?charset=utf8mb4"
@@ -693,7 +734,7 @@ def main():
 
     Session = sessionmaker(bind=engine)
 
-    # calling test function to do some transcation
+    # calling test function to do some transaction
     test(Session)
 
     # Deleting all tables
