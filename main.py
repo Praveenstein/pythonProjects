182e3c635f0ded092d0af35410a322d3f7942da7
Praveenstein <praveen06061995@gmail.com>
2020-09-30 17:23:32 +0530

    Importing engine from get connections first and using the ENGINE throught the code.

--- main.py
+++ main.py
@@ -7,16 +7,12 @@ environment you are running this script in.
     * sqlalchemy - Package used to connect to a database and do SQL operations using orm_queries
 """
 
-# External import
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-
 # User Import
+from library.connections.get_connection import ENGINE
 from library.orm.models import Base
 from library.populate.populate_db import populate
 from library.query.queries import basic_trans, impact_analysis,\
-    test_tamper, config_log_query
+    test_tamper
 
 
 __author__ = 'praveen@gyandata.com'
@@ -25,37 +21,27 @@ __author__ = 'praveen@gyandata.com'
 def main():
     """Main Function to populate the database and perform queries"""
 
-    # Creating engine to connect to Mysql database
-    url = "mysql+pymysql://root:nebula@localhost/library1?charset=utf8mb4"
-    engine = create_engine(url)
-
     # Creating all the tables
-    Base.metadata.create_all(engine)
-
-    # creating a session factory
-    Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(ENGINE)
 
     # Populating the database, # Giving a new session as parameter
-    populate(Session())
-
-    # Configuring the query log
-    config_log_query()
+    populate()
 
     # Calling the basic transaction to do some issues, passing the
     # Session factory as parameter
-    basic_trans(Session)
+    basic_trans()
 
     # Doing some student transactions and displaying the impact and other details,
     # And passing the Session factory as parameter
-    test_tamper(Session)
+    test_tamper()
 
     # Calling impact analysis to issue to books and check the impact
     # On Computer science department
     # And passing the session factory as parameter
-    impact_analysis(Session)
+    impact_analysis()
 
     # Deleting all tables
-    #Base.metadata.drop_all(engine)
+    Base.metadata.drop_all(ENGINE)
 
 
 if __name__ == '__main__':
